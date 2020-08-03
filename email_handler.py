5f55ddfc370a532c32ac076e5389abff489fdd31
mubeenghauri <p176107@nu.edu.pk>
2020-08-03 16:57:51 +0500

    Added thread wrapper for Emailer

--- email_handler.py
+++ email_handler.py
@@ -1,12 +1,14 @@
 import time
 import logging
 import smtplib, ssl
+from threading import Thread
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email_sheet_handler import EmailSheetHandler
 
-class _Emailer:
 
+class _Emailer:
+    """ Manage sending emails. """
     def __init__(self):
         self.emailHandler = EmailSheetHandler()
     
@@ -17,26 +19,21 @@ class _Emailer:
     def clean(self, file):
         with open(file, 'r') as f:
             L = f.readlines()
-
         cleaned = []
-
         count = 0
         for i in L:
             l = i.strip("\n").split(",")
             temp = []
             count += 1
-            
             for j in l:
                 if len(j) > 2:
                     temp.append(j)
             if len(temp)  >= 2:
                 cleaned.append(temp)
 
-
         with open(file, 'w') as f:
             for i in cleaned:
                 f.writelines(",".join(i)+"\n")
-
 
     def load_target_emails(self, csv):
         # self.clean(csv)
@@ -65,62 +62,16 @@ class _Emailer:
         with open(self.csv, 'w') as f:
             f.writelines(L)
 
-    
-    # def send_mails_without_name(self, msg, debug, type="html"):
-    #     logging.info("DEBUG = {}".format(debug))
-    #     logging.info("[WIHTOUT NAME] Initializing")
-    #     mail_index = 0
-    #     already_sent = 0
-    #     sent_count = 0
-    #     for creds in self.emails:
-    #         count = 0
-    #         logging.info("[*] Going through : "+creds[0]+" max-mails -> {}".format(creds[2]))
-    #         while count < int(creds[2]):
-    #             logging.info("mail_count = {} & count = {} & sent = {} & already_sent = {} & total_mails= {}".format(mail_index, count, sent_count, already_sent, len(self.target_emails) ))
-    #             sender_email = creds[0]
-    #             password = creds[1]
-    #             logging.info("[*] email -> {} pass -> {}".format(sender_email, password))
-    #             current = self.target_emails[mail_index]
-    #             reciever_mail = current[1].strip('\n')
-    #             if reciever_mail == '' or len(reciever_mail) == 0:
-    #                 logging.info("[WARNING] Skipping invalid bvalues : {}".format(current))
-    #                 mail_index+=1
-    #                 continue
-    #             if "SENT" in current:
-    #                 print("Already sent")
-    #                 logging.info("[INFO] {} :  already sent".format( reciever_mail))
-    #                 already_sent +=1
-    #                 mail_index+=1
-    #                 continue
-    #             logging.info("[*] Got mail : {} ".format(reciever_mail))
-    #             message = MIMEMultipart("alternative")
-    #             message["Subject"] = "Quick Question"
-    #             message["From"] = "Nabeel Ahmad Ghauri"
-    #             message["To"] = reciever_mail
 
-    #             m = MIMEText(msg, 'html')
-    #             # print(m)
-    #             message.attach(m)
-    #             context = ssl.create_default_context()
-                
-    #             if debug == 0:
-    #                 try:
-    #                     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
-    #                         server.login(sender_email, password)
-    #                         server.sendmail( sender_email, reciever_mail, message.as_string() )
-    #                         logging.info("Mail Sent to : "+reciever_mail)
-    #                         self.update_file(reciever_mail)
-    #                 except Exception as e:
-    #                         logging.exception("message")
-    #                         print("[WARNING] Some exception ....")
-    #                         print(e)
-            
-    #             if debug == 1: logging.info("[*] Sent to : {}".format(current))
-    #             count+=1
-    #             mail_index+=1
-    #             sent_count +=1
+class ThreadedEmaler(Thread, _Emailer):
+    """ Thread wrapper for Emailer. """
+    def __init__(self, msg, debug):
+        Thread.__init__(self)
+        _Emailer.__init__(self)
+        self.msg = msg 
+        self.debug = debug
 
-    def send_mails(self, msg, debug, type="html"):
+    def run(self):
         mail_index = 0
         already_sent = 0
         sent_count = 0
@@ -154,11 +105,11 @@ class _Emailer:
                 message["From"] = "Nabeel Ahmad Ghauri"
                 message["To"] = reciever_mail
 
-                m = MIMEText(msg.format(reciever_name), 'html')
+                m = MIMEText(self.msg.format(reciever_name), 'html')
                 message.attach(m)
                 context = ssl.create_default_context()
                 
-                if debug == 0:
+                if self.debug == 0:
                     try:
                         with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
                             server.login(sender_email, password)
@@ -169,8 +120,11 @@ class _Emailer:
                             logging.exception("message")
                             print("[WARNING] Some exception ....")
                             print(e)
-                else: logging.info("[*] Sent to : {}".format(current))
+                else: 
+                    logging.info("[*] Sent to : {}".format(current))
+                    time.sleep(2)
                 
                 count+=1
                 mail_index+=1
                 sent_count +=1
+        logging.info( "[DONE] Finished Sending mails !" )
